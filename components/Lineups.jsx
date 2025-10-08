2181ceb9e996bddcaf66fc895e1d91bd56a81f24
ulle73 <145575457+ulle73@users.noreply.github.com>
2025-10-09 01:23:01 +0200

    Add odds history component to lineups view

--- components/Lineups.jsx
+++ components/Lineups.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import useSWR from "swr";
+import TeamOddsHistory from "@/components/TeamOddsHistory";
 import { buildLineupsKey } from "@/lib/utils/apiKeys";
 import { fetchJson } from "@/lib/utils/fetchers";
 
@@ -640,7 +641,15 @@ export default function Lineups({ match, isLoading, className = "" }) {
           Lineups
         </h2>
       </div>
-      {content}
+      <div className="flex flex-1 min-h-0 flex-col">
+        <TeamOddsHistory
+          match={match}
+          className="flex-1 basis-[30%] min-h-0 border-b border-gray-100"
+        />
+        <div className="flex flex-[2] min-h-0 flex-col overflow-hidden">
+          {content}
+        </div>
+      </div>
     </div>
   );
 }
