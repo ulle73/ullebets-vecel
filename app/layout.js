0b706e93444e052ab155e9ebfd332b901361777d
ulle73 <145575457+ulle73@users.noreply.github.com>
2025-10-29 20:10:15 +0100

    Fix theme cascade specificity

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { ACTIVE_THEME } from "./theme-config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme={ACTIVE_THEME}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ app/theme-config.js
@@ -0,0 +1,10 @@
+export const AVAILABLE_THEMES = [
+  "dark-clean",
+  "dark-fancy",
+  "dark-animations",
+  "light-clean",
+  "light-fancy",
+  "light-animations",
+];
+
+export const ACTIVE_THEME = "dark-clean";
